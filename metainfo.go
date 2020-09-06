256a0e5402a7067919280be33afb26e8f3e6bc51
Wuhao Chen <wuhaochen@192.168.0.106>
2020-09-07 00:31:18 +0800

    implement info parsing

--- metainfo.go
+++ metainfo.go
@@ -14,6 +14,11 @@ type MetaInfo struct {
 
 type Info struct {
 	files []File
+	name string
+	length int
+	pieceLength int
+	pieces [][]byte
+	private int
 }
 
 type File struct {
@@ -62,6 +67,103 @@ func processMetaInfoInfo(rawMeta *map[string]bencode.BencodeCell, result *MetaIn
 
 	info.files = files
 
+	_ = processRawInfoName(&rawInfo, info)
+	_ = processRawInfoLength(&rawInfo, info)
+	_ = processRawInfoPrivate(&rawInfo, info)
+
+	err := processRawInfoPieceLength(&rawInfo, info)
+	if err != nil {
+		return MetaInfoError{"illegal rawInfo: Invalid Piece Length"}
+	}
+
+	err = processRawInfoPieces(&rawInfo, info)
+	if err != nil {
+		return MetaInfoError{"illegal rawInfo: Invalid Pieces"}
+	}
+
+	return nil
+}
+
+func processRawInfoPrivate(rawInfo *map[string]bencode.BencodeCell, info *Info) error {
+	rawPrivate, ok := (*rawInfo)["private"]
+	if !ok {
+		return MetaInfoError{"rawInfo does not contain private field"}
+	}
+
+	private, ok := rawPrivate.Value.(int)
+	if !ok {
+		return MetaInfoError{"rawInfor: private is not in valid form"}
+	}
+
+	info.private = private
+	return nil
+}
+
+func processRawInfoPieces(rawInfo *map[string]bencode.BencodeCell, info *Info) error {
+	rawPieces, ok := (*rawInfo)["pieces"]
+	if !ok {
+		return MetaInfoError{"rawInfo does not contain pieces field"}
+	}
+
+	pieces, ok := rawPieces.Value.(string)
+	if !ok {
+		return MetaInfoError{"rawInfo: pieces is not in valid form"}
+	}
+
+	piecesB := []byte(pieces)
+	chunkCount := len(piecesB) / 20
+	info.pieces = make([][]byte, chunkCount)
+
+	for i := 0; i < chunkCount; i++ {
+		tmp := make([]byte, 0, 20)
+		info.pieces[i] = append(tmp, piecesB[i * 20 : i * 20 + 20]...)
+	}
+
+	return nil
+}
+
+func processRawInfoPieceLength(rawInfo *map[string]bencode.BencodeCell, info *Info) error {
+	rawPieceLength, ok := (*rawInfo)["piece length"]
+	if !ok {
+		return MetaInfoError{"rawInfo does not contain piece field"}
+	}
+
+	pieceLength, ok := rawPieceLength.Value.(int)
+	if !ok {
+		return MetaInfoError{"rawInfo: piece length is not in valid form"}
+	}
+
+	info.pieceLength = pieceLength
+	return nil
+}
+
+func processRawInfoLength(rawInfo *map[string]bencode.BencodeCell, info *Info) error {
+	rawLength, ok := (*rawInfo)["length"]
+	if !ok {
+		return MetaInfoError{"rawInfo does not contain length field"}
+	}
+
+	length, ok := rawLength.Value.(int)
+	if !ok {
+		return MetaInfoError{"rawInfo: length is not in valid form"}
+	}
+
+	info.length = length
+	return nil
+}
+
+func processRawInfoName(rawInfo *map[string]bencode.BencodeCell, info *Info) error {
+	rawName, ok := (*rawInfo)["name"]
+	if !ok {
+		return MetaInfoError{"rawInfo does not contain name field"}
+	}
+
+	name, ok := rawName.Value.(string)
+	if !ok {
+		return MetaInfoError{"rawInfo: name is not in valid form"}
+	}
+
+	info.name = name
 	return nil
 }
 
@@ -85,7 +187,7 @@ func UnmarshalMetaInfoFrom(rawMeta map[string]bencode.BencodeCell) (*MetaInfo, e
 		result.announceList = announceList
 	}
 
-	processMetaInfoInfo(&rawMeta, result)
+	_ = processMetaInfoInfo(&rawMeta, result)
 
 	return result, nil
 }
@@ -109,4 +211,5 @@ func main() {
 	//fmt.Printf("%v\n", metainfo)
 	r, err := UnmarshalMetaInfoFrom(metainfo.(map[string]bencode.BencodeCell))
 	fmt.Println(r)
+
 }
